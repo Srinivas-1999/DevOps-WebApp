e81034245245c2103039f65059afa14bd5d182a9
srinivaslogisa99@gmail.com <srinivaslogisa99@gmail.com>
2025-05-04 19:33:16 +0530

    changed files

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "This is ASP.NET Creating for UI based on websites");
+app.MapGet("/", () => "This is ASP.NET Creating for UI based on web");
 
 app.Run();
