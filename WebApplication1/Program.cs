62f3edd3418345da81f2f7a1349f1e23adf947d0
srinivaslogisa99@gmail.com <srinivaslogisa99@gmail.com>
2025-05-04 19:26:07 +0530

    Updated Program.cs file

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "This is ASP.NET test web application");
+app.MapGet("/", () => "This is ASP.NET Creating for UI based on websites");
 
 app.Run();
