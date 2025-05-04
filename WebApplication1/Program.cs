49c994ad7589e7486353048a2ff7929ef38560ab
Srinivas-1999 <124429890+Srinivas-1999@users.noreply.github.com>
2025-05-04 19:46:40 +0530

    Update Program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "This is ASP.NET Creating for UI based on web");
+app.MapGet("/", () => "This is ASP.NET Creating for UI based on web application");
 
 app.Run();
